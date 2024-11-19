aa8576d74c6130b2f7f1a43c8d672601822b15e7
aditykumar5252 <aditykumar5252@gmail.com>
2024-11-19 13:51:34 +0530

    Update HotelReservationSystem.java

--- Hotel Reservation System/HotelReservationSystem.java
+++ Hotel Reservation System/HotelReservationSystem.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 public class HotelReservationSystem {
     private static final String url = "jdbc:mysql://localhost:3306/Hotel_db";
     private static final String username = "root";
-    private static final String password = "Golu@120103";
+    private static final String password = "Admin-Pass";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         try{
